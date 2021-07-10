a4242d02ac77bd38340f7b7cdf98c0fef4367bc3
Prasantacharya <acharp@rpi.edu>
2021-07-10 12:21:19 -0400

    trying to test some more stuff with the news functionality. Probably will pull it into a webhook or something

--- bot.py
+++ bot.py
@@ -5,28 +5,23 @@ import yaml
 import discord
 from discord.ext import commands
 
-def run():
-    # gets the config file
-    with open("config.yaml", 'r') as f:
-        try:
-            config = yaml.safe_load(f)
-        except yaml.YAMLError as exc:
-            print("Could not load config")
-            return
-    print("Successfully loaded config!\n")    
-    # bot token here
-    token = os.getenv('DISCORD_BOT_TOKEN')
-    print(f"token: {token}")
-    print(config["prefix"])
-    bot = commands.Bot(command_prefix=config["prefix"])
-    # add in the cogs here
-    for cog in config['cogs']:
-        print(cog) # bot.load_extension("cogs." + config[cog]) 
-    bot.load_extension("cogs.help")
-    bot.run("ODA1NTM3ODAwMTkxMDgyNTA2.YBcViQ.QWuXr4xfLgO8JZhHzroUbU-oxBA")
-    print("exiting")
+with open("config.yaml", 'r') as f:
+    try:
+        config = yaml.safe_load(f)
+    except yaml.YAMLError as exc:
+        sys.exit("Invalid config")
 
+print("Successfully loaded config!\n")    
 
-if __name__ == '__main__':
-    # parse the config file
-    run()
+# bot token here
+token = os.getenv('DISCORD_BOT_TOKEN')
+bot = commands.Bot(command_prefix=config["prefix"])
+
+print("loading cogs")
+# add in the cogs here
+for cog in config['cogs']:
+    print(cog) # bot.load_extension("cogs." + config[cog]) 
+    bot.load_extension(f"cogs.{cog}")
+
+bot.run(config["token"])
+
